226bebee353a8447236bf0b9a830e53480faa03c
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-03-07 15:09:50 +0100

    corrected some fastidious incongruities that were giving me headache

--- scripts.js
+++ scripts.js
@@ -5,11 +5,20 @@ const gridContainer = document.querySelector(".gridContainer");
 function createGrid(size) {
     const fragment = document.createDocumentFragment();
     gridContainer.innerHTML= "";
+    const totalGridSize = 512;
+    const baseGridSize = 512; 
+    const maxGridSize = Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8); 
+    const completeGridSize = maxGridSize; 
+    const cellSize = completeGridSize / size;
+    gridContainer.style.width = `${completeGridSize}px`;
+    gridContainer.style.height = `${completeGridSize}px`;
 
     for (let rows = 0; rows < size; rows++) {
         for (let columns = 0; columns < size; columns++) {
             const gridCell = document.createElement("div");
             gridCell.classList.add("gridCell");
+            gridCell.style.width = `${cellSize}px`;
+            gridCell.style.height = `${cellSize}px`;
             gridCell.addEventListener("mouseover", function() {
             gridCell.classList.add("hovered");
             });
