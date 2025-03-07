3e0785924f7dacf86ebeac4eac74f4a1816fb7d1
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-03-07 14:26:30 +0100

    finished the etch-a-sketch project by using css, js, and html

--- scripts.js
+++ scripts.js
@@ -1,13 +1,31 @@
 const gridSize= 16;
 const gridContainer = document.querySelector(".gridContainer");
-const fragment = document.createDocumentFragment();
 
-for (let rows = 0; rows < gridSize; rows++) {
-    for (let columns = 0; columns < gridSize; columns++) {
-        const gridCell = document.createElement("div");
-        fragment.appendChild(gridCell);
-        gridCell.classList.add("gridCell");
+
+function createGrid(size) {
+    const fragment = document.createDocumentFragment();
+    gridContainer.innerHTML= "";
+
+    for (let rows = 0; rows < size; rows++) {
+        for (let columns = 0; columns < size; columns++) {
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("gridCell");
+            gridCell.addEventListener("mouseover", function() {
+            gridCell.classList.add("hovered");
+            });
+            fragment.appendChild(gridCell);
+        }
+        
     }
-    
-} 
-gridContainer.appendChild(fragment);
+    gridContainer.appendChild(fragment);
+}
+createGrid(gridSize);
+
+document.querySelector("#gridBtn").addEventListener("click", () => {
+    let userGrid = prompt("Input a number between 1 and 100:");
+    while (isNaN(userGrid) || userGrid < 1 || userGrid > 100 || userGrid === "") {
+        userGrid = prompt("Invalid input! Enter a number between 1 and 100:")        
+    }
+    userGrid = Number(userGrid);
+    createGrid(userGrid);
+});
